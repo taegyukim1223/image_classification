468a8ac3409a74e1ec27975ba66006f307e8d283
steinstein <81689449+taegyukim1223@users.noreply.github.com>
2022-02-22 17:52:00 +0900

    Add files via upload
    version1.1

--- train.py
+++ train.py
@@ -16,22 +16,15 @@ parser.add_argument('--epoch', type=int,   default=50)
 parser.add_argument('--batch_size', type=int,   default=32)
 parser.add_argument('--lr', type=float, default=0.001)
 parser.add_argument('--resolution', type=int, default=256)
-parser.add_argument('--data_path', type=str, default='')
+parser.add_argument('--data_path', type=str, default=os.path.dirname(os.path.realpath(__file__)))
 parser.add_argument('--model_save_name', type=str, default='temp')
 parser.add_argument('--gpu_num', type=int, default=0)
 parser.add_argument('--gpus', type=int, nargs='+', default = 0, help = 'type the gpus index when using the multi-gpus')
-parser.add_argument('--class_num', type=int, help = 'The number of classes.', default = 0)
-parser.add_argument('--model_load', type=str, help = 'model Name of Timm library or your own model path', default='inception_v4')
+parser.add_argument('--class_num', type=int, help = 'The number of classes.', default = 2)
+parser.add_argument('--model_load', type=str, help = 'model Name of Timm library or your own model path', default='resnet34')
 args = parser.parse_args()
 
-gpu_list = args.gpus
 resolution = (args.resolution, args.resolution) 
-data_dir = args.data_path
-num_epochs = args.epoch
-modelfile_save_name = args.model_save_name
-train_batch_size = args.batch_size
-class_num = args.class_num
-
 
 device = torch.device(f'cuda:{args.gpu_num}' if torch.cuda.is_available() else 'cpu')
 torch.cuda.set_device(device) # change allocation of current GPU
@@ -50,7 +43,6 @@ if device.type == 'cuda':
 transforms_train = transforms.Compose([
     transforms.Resize(resolution),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomResizedCrop((224, 224), scale=(0.1, 1), ratio=(0.5, 2)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # 정규화(normalization)
 ])
@@ -61,24 +53,15 @@ transforms_val = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # 정규화(normalization)
 ])
 
-transforms_test = transforms.Compose([
-    transforms.Resize(resolution),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
 
-
-train_datasets = datasets.ImageFolder(os.path.join(data_dir, 'train'), transforms_train)
-val_datasets = datasets.ImageFolder(os.path.join(data_dir, 'val'), transforms_val)
-test_datasets = datasets.ImageFolder(os.path.join(data_dir, 'test'), transforms_test)
-
-train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=train_batch_size, shuffle=True)
+train_datasets = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transforms_train)
+val_datasets = datasets.ImageFolder(os.path.join(args.data_path, 'val'), transforms_val)
+train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=args.batch_size, shuffle=True)
 val_dataloader = torch.utils.data.DataLoader(val_datasets, batch_size=1, shuffle=True)
-test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=1, shuffle=True)
 
 print('학습 데이터셋 크기:', len(train_datasets))
-print('테스트 데이터셋 크기:', len(test_datasets))
 print('valid 데이터셋 크기:', len(val_datasets))
+    
 
 class_names = train_datasets.classes
 print('클래스:', class_names)
@@ -88,15 +71,13 @@ map_location = 'cuda:{}'.format(args.gpu_num)
 try :
     model = torch.load(args.model_load, map_location=map_location)
 except :
-    model = timm.create_model(args.model_load, pretrained=True)
-    num_features = model.last_linear.in_features
-#    전이 학습(transfer learning): 모델의 출력 뉴런 수를 2개로 교체하여 마지막 레이어 다시 학습
-    model.fc = nn.Linear(num_features, args.class_num)
-    
+    #전이 학습(transfer learning): 모델의 출력 뉴런 수를 클래스 갯수로 교체하여 마지막 레이어 다시 학습
+    model = timm.create_model(args.model_load, pretrained=True, num_classes=args.class_num)
+
 print(model)
 
 if args.gpus != 0 :
-    model = torch.nn.DataParallel(model, device_ids=gpu_list) # GPU병렬연결
+    model = torch.nn.DataParallel(model, device_ids=args.gpus) # GPU병렬연결
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
 model = model.to(device)
@@ -106,7 +87,7 @@ model.train()
 start_time = time.time()
 
 # 전체 반복(epoch) 수 만큼 반복하며
-for epoch in range(num_epochs):
+for epoch in range(args.epoch):
     running_loss = 0.
     running_corrects = 0
 
@@ -142,7 +123,7 @@ for epoch in range(num_epochs):
         running_loss = 0.
         running_corrects = 0
 
-        for inputs, labels in test_dataloader:
+        for inputs, labels in val_dataloader:
             inputs = inputs.to(device)
             labels = labels.to(device)
 
@@ -158,5 +139,5 @@ for epoch in range(num_epochs):
         print('[Test Phase] Loss: {:.4f} Acc: {:.4f}% Time: {:.4f}s'.format(epoch_loss, epoch_acc, time.time() - start_time))
         
         if acc_past < epoch_acc :
-            torch.save(model,'{}.pth'.format(modelfile_save_name))
+            torch.save(model,'{}.pth'.format(args.model_save_name))
             acc_past = epoch_acc
